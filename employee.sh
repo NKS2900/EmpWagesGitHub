fe8fd891b7a74d6db09cd02b3fc2b665f540ca0a
Nijam Sayyad <58353284+NKS2900@users.noreply.github.com>
2020-10-11 23:16:27 +0530

    Added: Calculate EmpWages Using Switch Case

--- employee.sh
+++ employee.sh
@@ -3,16 +3,18 @@
 empRatePerHour=20;
 isFullTime=1;
 isPartTime=2;
-randomCheck=$((RANDOM%3))
+empCheck=$((RANDOM%3))
 
-if [ $randomCheck -eq $isFullTime ]
-then	
-	empHrs=8;
-elif [ $randomCheck -eq $isPartTime ]
-then
-	empHrs=4;
-else
-	empHrs=0;
-fi
+case $empCheck in 
+	$isFullTime)
+		empHrs=8
+		;;
+	$isPartTime)
+		empHrs=4
+		;;
+	*)
+		empHrs=0
+		;;
+esac
 
 salary=$(($empHrs*$empRatePerHour));
