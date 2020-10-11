81652b24bc409d00662c4b3f4fe56d3801ef988f
Nijam Sayyad <58353284+NKS2900@users.noreply.github.com>
2020-10-11 23:13:04 +0530

    Update employee.sh

--- employee.sh
+++ employee.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -x
+
+isFullTime=1
 randomCheck=$((RANDOM%2))
 
-if [ $randomCheck -eq 1 ]
+if [ $randomCheck -eq $isFullTime ]
 then
-	echo "Employee is present"
+	empRatePerHour=20;
+	empHrs=8;
+	salary=$(($empHrs*$empRatePerHour));
 else
-	echo "Employee is absent"
+	salary=0;
 fi
