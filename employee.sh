477d86d6e85c65a21d31a17d0d667240479e25c7
Nijam Sayyad <58353284+NKS2900@users.noreply.github.com>
2020-10-11 23:39:07 +0530

    Added: calEmpWages using Dictionar

--- employee.sh
+++ employee.sh
@@ -1,1 +1,45 @@
+#!/bin/bash -x
+
+IS_FULL_TIME=1;
+IS_PART_TIME=2;
+EMP_RATE_PER_HOUR=20;
+NUM_OF_WORKING_DAYS=20;
+MAX_HOURS_IN_MONTH=40;
+
+totalEmpHours=0;
+totalWorkingDays=0;
+
+declare	-A dailyWage
+
+function getWorkingHours(){
+	case $1 in 
+		$IS_FULL_TIME )
+			empHrs=8
+			;;
+		$IS_PART_TIME )
+			empHrs=4
+			;;
+		* )
+			empHrs=0
+			;;
+	esac
+	echo $empHrs
+}
+
+function getEmpWage(){
+	empHours=$1
+	echo $(($empHours*$EMP_RATE_PER_HOUR))
+}
+
+while [[ $totalEmpHours -lt $MAX_HOURS_IN_MONTH &&  $totalWorkingDays -lt $NUM_OF_WORKING_DAYS ]]
+do
+	((totalWorkingDays++))
+	empHours=$( getWorkingHours $((RANDOM%3)) )	
+	totalEmpHours=$(($totalEmpHours+$empHours))
+	dailyWage[$totalWorkingDays]=$( getEmpWage $empHours )
+done
+
+totalSalary=$(($totalEmpHours*$EMP_RATE_PER_HOUR));
+echo "Employee Daily Wages : " ${dailyWage[@]}
+echo "Employee Working Days : " ${!dailyWage[@]}
 
