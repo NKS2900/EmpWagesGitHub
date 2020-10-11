9c7a72f2d52c82137aab10a98540d05e9d3eb3c9
Nijam Sayyad <58353284+NKS2900@users.noreply.github.com>
2020-10-11 23:17:48 +0530

    Added: Calculate EmpMothly Wages

--- employee.sh
+++ employee.sh
@@ -1,20 +1,28 @@
 #!/bin/bash -x
 
-empRatePerHour=20;
 isFullTime=1;
 isPartTime=2;
-empCheck=$((RANDOM%3))
+empRatePerHour=20;
+numOfWorkingDays=20;
+totalSalary=0;
 
-case $empCheck in 
-	$isFullTime)
-		empHrs=8
-		;;
-	$isPartTime)
-		empHrs=4
-		;;
-	*)
-		empHrs=0
-		;;
-esac
+for ((day=1; day<=$numOfWorkingDays; day++))
+do
+	empCheck=$((RANDOM%3))
+
+	case $empCheck in 
+		$isFullTime)
+			empHrs=8
+			;;
+		$isPartTime)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+
+done
 
 salary=$(($empHrs*$empRatePerHour));
+totalSalary=$(($totalSalary+$salary))
